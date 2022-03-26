de3611e942e91fc188c9a7e6e6365c7fc4fa6722
Camila Storck <mcamilastorck@gmail.com>
2022-03-25 22:49:39 -0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Apple  on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
